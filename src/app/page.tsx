5bef2f2e6067004ba3df50104b72d3feeadf3db0
LearnShare <learnshare@126.com>
2024-11-02 15:26:30 +0800

    create project with Next.js.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="fixed inset-0 p-6 h-full flex flex-col gap-8 items-center justify-center">
+      <h1 className="text-3xl font-bold text-slate-800">Twinkle</h1>
+      <p className="text-lg text-sky-500">Twinkle Twinkle 🌟</p>
+    </div>
+  );
+}
