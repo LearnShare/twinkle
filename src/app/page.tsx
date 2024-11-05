cb77f6895e487fe8d24fe7eddc6f3cac4580f857
LearnShare <learnshare@126.com>
2024-11-05 21:50:20 +0800

    feat: mosaic style background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="fixed inset-0 p-6 h-full flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-3xl font-bold text-slate-800">Twinkle</h1>
-      <p className="text-lg text-sky-500">Twinkle Twinkle 🌟</p>
+      <div className="bg-[url('/assets/pattern.svg')] bg-clip-text text-[100px] font-extrabold text-transparent">Twinkle Twinkle 🌟</div>
     </div>
   );
 }
